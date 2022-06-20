863711b3ae7f72df420b071468e0f60f79c741f4
iSweazZ <wissem26500@gmail.com>
2022-06-20 16:33:24 +0200

    ajout d'un bouton "comment avoir mon token"

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -10,6 +10,7 @@
                     <label for="discordToken">Votre token Discord</label><br>
                     <input type="text" name="value" class="text-black w-full rounded-md" id="discordToken"><br>
                     <div class="flex items-center justify-end mt-4 mb-4">
+                        <a class="font-bold text-white bg-gradient-to-tr from-violet-900 to-rose-600 rounded-md pt-2 pb-2 pr-4 pl-4 mr-2" name="type" value="slackToken" href="https://serveur-discord.com/news/tutoriel-ajouter-webhook-discord" target="_blank">Comment avoir mon teken ?</a>
                         <button class="font-bold text-white bg-gradient-to-tr from-violet-900 to-rose-600 rounded-md pt-2 pb-2 pr-4 pl-4" name="type" value="discordToken">Valider</button>
                     </div>
                 </form>
@@ -19,6 +20,7 @@
                     <label for="slackToken">Votre token Slack</label><br>
                     <input type="text" name="value" id="slackToken" class="text-black w-full rounded-md"><br>
                     <div class="flex items-center justify-end mt-4 mb-4">
+                        <a class="font-bold text-white bg-gradient-to-tr from-violet-900 to-rose-600 rounded-md pt-2 pb-2 pr-4 pl-4 mr-2" name="type" value="slackToken" href="https://slack.com/intl/fr-fr/help/articles/115005265063-Webhooks-entrants-pour-Slack" target="_blank">Comment avoir mon teken ?</a>
                         <button class="font-bold text-white bg-gradient-to-tr from-violet-900 to-rose-600 rounded-md pt-2 pb-2 pr-4 pl-4" name="type" value="slackToken">Valider</button>
                     </div>
                 </form>
