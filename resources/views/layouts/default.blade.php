a3b6c04c23d49f642a73cc273866532af242a4e5
iSweazZ <wissem26500@gmail.com>
2022-06-20 15:36:45 +0200

    fin de prrojet

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,14 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('customHead')
 </head>
-<body class="bg-zinc-900 overflow-x-hidden">
-<h1 id="appTitleDashboard" class="text-white customTextSize3vw text-center">{{config('app.name', 'Laravel')}} - {{ucfirst($title ?? '')}}</h1>
+<body class="bg-zinc-900 overflow-x-hidden ">
+    @if ($title == 'Accueil')
+    <!-- <h1 id="appTitleDashboard" class="text-transparent customTextSize3vw text-center bg-clip-text z-50 bg-gradient-to-tr from-violet-900 to-rose-600 font-bold">{{config('app.name', 'Laravel')}} - {{ucfirst($title ?? '')}}</h1> -->
+    <h1 id="appTitleDashboard" class="text-rose-600 customTextSize3vw text-center font-bold mt-2">{{config('app.name', 'Laravel')}} - {{ucfirst($title ?? '')}}</h1>
+    @else
+    <h1 id="appTitleDashboard" class="text-white customTextSize3vw text-center font-bold mt-2" >{{config('app.name', 'Laravel')}} - {{ucfirst($title ?? '')}}</h1>
+        
+    @endif
     @yield('content')
 </body>
 </html>
